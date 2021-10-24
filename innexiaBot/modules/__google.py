b8faaee64e719b17a4e6ea6782cab3fcea673875
Rishabhbhan5 <90978799+Rishabhbhan5@users.noreply.github.com>
2021-10-24 08:26:54 +0530

    Update __google.py

--- innexiaBot/modules/__google.py
+++ innexiaBot/modules/__google.py
@@ -267,7 +267,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> @SiderzBot <==="
+        app_details += "\n\n===> @RishabhBotsHelp <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
@@ -286,6 +286,6 @@ __help__ = """
  ❍ /github <username>*:* Get information about a GitHub user.
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
- ❍ Innexia <query>*:* Innexia answers the query
-  💡Ex: `Innexia where is India?`
+ ❍ Jennie <query>*:* Jennie answers the query
+  💡Ex: `Jennie where is India?`
 """
